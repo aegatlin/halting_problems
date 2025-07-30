406a7cbbb0a4dd663f64c988ca3875e28d30ab90
Austin Gatlin <austin@gatlin.io>
2025-07-30 13:44:48 +1000

    update factors functions

--- rsgames/src/factors.rs
+++ rsgames/src/factors.rs
@@ -1,4 +1,6 @@
-/// All factors include `1` and `n`.
+use std::collections::HashSet;
+
+/// Returns all factors (including `1` and `n`).
 pub fn get_all_factors(n: u64) -> Vec<u64> {
     let mut factors: Vec<u64> = Vec::new();
 
@@ -11,6 +13,7 @@ pub fn get_all_factors(n: u64) -> Vec<u64> {
     factors
 }
 
+/// Returns all the proper factors of `n`. Proper factors exclude `1` and `n`.
 pub fn get_proper_factors(n: u64) -> Vec<u64> {
     let all_factors = get_all_factors(n);
 
@@ -18,6 +21,51 @@ pub fn get_proper_factors(n: u64) -> Vec<u64> {
         .into_iter()
         .filter(|&x| x != 1 && x != n)
         .collect()
+}
+
+/// Returns all the sets of numbers whose products are `n`. The set includes
+/// `vec![n]`. The set excludes `1`s, since those could be infinitely added.
+///
+/// # Example
+///
+/// ```
+/// use rsgames::factors::get_multiplicative_partitions;
+/// use std::collections::HashSet;
+///
+/// let actual = get_multiplicative_partitions(20);
+/// let expected = HashSet::from([vec![2, 2, 5], vec![2, 10], vec![4, 5], vec![20]]);
+/// assert_eq!(actual, expected);
+/// ```
+///
+/// # Notes
+///
+/// - https://oeis.org/A001055
+/// - https://en.wikipedia.org/wiki/Multiplicative_partition
+pub fn get_multiplicative_partitions(n: u64) -> HashSet<Vec<u64>> {
+    let proper_factors = get_proper_factors(n);
+
+    // base case
+    if proper_factors.is_empty() {
+        return HashSet::from([vec![n]]);
+    }
+
+    // recursive case
+    let mut partitions = proper_factors
+        .iter()
+        .flat_map(|&f| {
+            get_multiplicative_partitions(n / f)
+                .into_iter()
+                .map(|mut sub_partition| {
+                    sub_partition.push(f);
+                    sub_partition.sort();
+                    sub_partition
+                })
+                .collect::<Vec<Vec<u64>>>()
+        })
+        .collect::<HashSet<Vec<_>>>();
+
+    partitions.insert(vec![n]);
+    partitions
 }
 
 #[cfg(test)]
@@ -34,5 +82,29 @@ mod test {
     fn test_get_proper_factors() {
         assert_eq!(get_proper_factors(10), vec![2, 5]);
         assert_eq!(get_proper_factors(11), vec![]);
+    }
+
+    #[test]
+    fn test_multiplicative_partitions_of() {
+        let expected = HashSet::from([vec![19]]);
+        assert_eq!(get_multiplicative_partitions(19), expected);
+
+        let expected2 = HashSet::from([vec![2, 2, 5], vec![2, 10], vec![4, 5], vec![20]]);
+        assert_eq!(get_multiplicative_partitions(20), expected2);
+
+        let expected3 = HashSet::from([
+            vec![60],
+            vec![2, 30],
+            vec![3, 20],
+            vec![4, 15],
+            vec![5, 12],
+            vec![6, 10],
+            vec![2, 2, 15],
+            vec![2, 3, 10],
+            vec![2, 5, 6],
+            vec![3, 4, 5],
+            vec![2, 2, 3, 5],
+        ]);
+        assert_eq!(get_multiplicative_partitions(60), expected3);
     }
 }
--- rsgames/src/obscura.rs
+++ rsgames/src/obscura.rs
@@ -1,3 +1,8 @@
+///
+/// Obscura is the home of functionality that I suspect I will never use again.
+///
+use crate::factors;
+
 /// A product-sum number is one that can be constructed from a set of numbers
 /// using both product and sum.
 ///
@@ -6,13 +11,26 @@
 ///
 /// Product-sum numbers can have multiple sets that construct them, e.g.,
 /// 1,1,2,4 and 1,1,2,2,2 for the number 8.
-pub fn product_sum(number_set: Vec<u64>) -> Option<u64> {
-    let sum: u64 = number_set.iter().sum();
-    let product: u64 = number_set.iter().product();
-    if sum == product {
-        return Some(sum);
+pub fn get_product_sums(n: u64) -> Vec<Vec<u64>> {
+    let mut product_sums: Vec<Vec<u64>> = Vec::new();
+    let mps = factors::get_multiplicative_partitions(n);
+
+    for mp in mps {
+        if mp.len() == 1 && mp.first() == Some(&n) {
+            continue;
+        }
+
+        let sum: u64 = mp.iter().sum();
+        let ones_count = n - sum;
+        if ones_count >= 0 {
+            let mut answer = vec![1; ones_count as usize];
+            answer.extend(mp);
+            product_sums.push(answer);
+        }
     }
-    None
+
+    product_sums.sort();
+    product_sums
 }
 
 #[cfg(test)]
@@ -20,8 +38,12 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_product_sum() {
-        assert_eq!(product_sum(vec![1, 1, 2, 4]), Some(8));
-        assert_eq!(product_sum(vec![1, 1, 2, 5]), None);
+    fn test_get_product_sums() {
+        assert_eq!(get_product_sums(2), vec![] as Vec<Vec<u64>>);
+        assert_eq!(get_product_sums(4), vec![vec![2, 2]]);
+        assert_eq!(
+            get_product_sums(8),
+            vec![vec![1, 1, 2, 2, 2], vec![1, 1, 2, 4]]
+        );
     }
 }
