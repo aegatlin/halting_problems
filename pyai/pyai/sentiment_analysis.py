79b06b9b7c4bd4b7be58733ab9535a15297cbc42
Austin Gatlin <austin@gatlin.io>
2025-11-08 15:20:07 +1100

    remove pyai

--- pyai/pyai/sentiment_analysis.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-classifier = pipeline("sentiment-analysis", device="mps")
-
-results = classifier("We are very happy to show you the 🤗 Transformers library.")
-
-print(results)
