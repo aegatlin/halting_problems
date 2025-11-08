6c2d73e7597056265b6a544fd0df34ac792acb99
Austin Gatlin <austin@gatlin.io>
2025-11-08 15:23:01 +1100

    rename pyai2 to pyai and add old stuff to rsgames

--- pyai/main.py
+++ pyai/main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from pyai2!")
+    print("Hello!")
 
 
 if __name__ == "__main__":
--- rsgames/src/main.rs
+++ rsgames/src/main.rs
@@ -1,1 +1,28 @@
-fn main() {}
+use rsgames::words::WordProperties;
+
+fn main() {
+    let contents = rsgames::files::read_file("0098_words.txt");
+    let words = rsgames::strings::csv_string_to_list(&contents);
+    let words2: Vec<&str> = words
+        .into_iter()
+        .map(|word| word.trim_matches('"'))
+        .collect();
+
+    for word in &words2 {
+        let mut anagrams: Vec<&str> = Vec::new();
+
+        for other_word in &words2 {
+            if word == other_word {
+                continue;
+            }
+
+            if word.is_anagram(other_word) {
+                anagrams.push(other_word);
+            }
+        }
+
+        if anagrams.len() > 0 {
+            println!("{}: {:?}", word, anagrams);
+        }
+    }
+}
